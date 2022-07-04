87418a4985e0f43ee43a3b4291a837954db0d329
OkoroaforKelechiDivine <106816493+OkoroaforKelechiDivine@users.noreply.github.com>
2022-07-04 18:14:01 +0100

    Fixing and arranging files

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>Hello</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LandingPage"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/LandingPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <p>How are you doing?</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LandingPage"
+}
+</script>
+
+<style scoped>
+
+</style>
